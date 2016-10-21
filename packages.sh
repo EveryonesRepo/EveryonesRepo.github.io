f432f9d6496e1997d8e5b907da179fe3388db81b
Avery Pope <avery@Averys-MacBook-Pro.local>
2016-10-20 21:54:03 -0400

    init

--- packages.sh
+++ packages.sh
@@ -2,3 +2,6 @@
 dpkg-deb -bZgzip projects/alkaline debs
 dpkg-deb -bZgzip projects/afc2d debs
 dpkg-deb -bZgzip projects/k10 debs
+dpkg-deb -bZgzip projects/betterfiveicondock debs
+dpkg-deb -bZgzip projects/betterfivecolumnhomescreen debs
+dpkg-deb -bZgzip projects/CrashReporter debs
