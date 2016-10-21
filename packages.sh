41a0535e22b123a51a72de047edbbf35df87d72e
Avery Pope <avery@Averys-MacBook-Pro.local>
2016-10-20 22:01:29 -0400

    init

--- packages.sh
+++ packages.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/alkaline debs
-dpkg-deb -bZgzip projects/afc2d debs
-dpkg-deb -bZgzip projects/k10 debs
-dpkg-deb -bZgzip projects/betterfiveicondock debs
-dpkg-deb -bZgzip projects/betterfivecolumnhomescreen debs
-dpkg-deb -bZgzip projects/CrashReporter debs
+dpkg-deb -bZgzip projects/BetterTextSelection debs
